ba054de00fa6084d09e7e4685ae02a204b5b83ad
Surendra Pathak <Surendra.Pathak@interlynk.io>
2023-04-17 19:12:04 -0700

    [sbomex v0.0.3] Release Update

--- Formula/sbomex.rb
+++ Formula/sbomex.rb
@@ -1,11 +1,11 @@
 # Copyright 2023 Interlynk.io
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,21 +16,21 @@
 class Sbomex < Formula
   desc "sbomex is a command line utility to help query and pull from Interlynk public SBOM"
   homepage "https://github.com/interlynk-io/sbomex"
-  version "0.0.2"
+  version "0.0.3"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/interlynk-io/sbomex/releases/download/v0.0.2/sbomex-darwin-amd64", :using => :nounzip
-      sha256 "761678fb0aa5597c5993ff7a7e9a188f06d830649a8798fa1b983a5fcabc0342"
+      url "https://github.com/interlynk-io/sbomex/releases/download/v0.0.3/sbomex-darwin-amd64", :using => :nounzip
+      sha256 "9546707130adb82e9a78b12fbeff6cb512d7c7a100253db25cfa3eb0f147b545"
 
       def install
         bin.install "sbomex-darwin-amd64" => "sbomex"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/interlynk-io/sbomex/releases/download/v0.0.2/sbomex-darwin-arm64", :using => :nounzip
-      sha256 "4ea732ec1633e69c7ce2644f6bb4b19ebf0ae94849e5e78b37a356b26d9e548e"
+      url "https://github.com/interlynk-io/sbomex/releases/download/v0.0.3/sbomex-darwin-arm64", :using => :nounzip
+      sha256 "4c218bb45f8e9f24223665ec67f797b7bf1f42a817899da067a61cf912c5a373"
 
       def install
         bin.install "sbomex-darwin-arm64" => "sbomex"
@@ -40,8 +40,8 @@ class Sbomex < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/interlynk-io/sbomex/releases/download/v0.0.2/sbomex-linux-amd64", :using => :nounzip
-      sha256 "8f6e3e6a33032df5626a690fc13defeaa45ee5d4aebb043328fb92c65e795b0e"
+      url "https://github.com/interlynk-io/sbomex/releases/download/v0.0.3/sbomex-linux-amd64", :using => :nounzip
+      sha256 "83f718bfd9a8e572733947974b6d574aed8df4a99737f1ec0694c18a4dd7edc9"
 
       def install
         bin.install "sbomex-linux-amd64" => "sbomex"
